8b7fa906708127102232a4dd14254feaa718ebfe
Aliaksandr Vasilenka <Aliaksandr.Vasilenka@ventionteams.com>
2026-04-24 12:35:49 +0200

    Fixes linter warnings

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -143,6 +143,8 @@ use serde::{Deserialize, Serialize};
 pub use ed25519_dalek::{Signature, SigningKey, VerifyingKey};
 pub use rand::rngs::OsRng;
 
+mod error;
+
 /// The category of error returned by a JWT operation
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum ErrorKind {
@@ -529,13 +531,13 @@ impl<'a, H: 'a, A: 'a, C: 'a> Iterator for Iter<'a, H, A, C> {
     fn next(&mut self) -> Option<Self::Item> {
         let claims = self.claims?;
         let item = (&claims.host, &claims.actor_id, &claims.claims);
-        self.claims = claims.inherit.as_ref().map(|claims| &**claims);
+        self.claims = claims.inherit.as_deref();
         Some(item)
     }
 }
 
 impl<H, A, C> Claims<H, A, C> {
-    pub fn iter(&self) -> Iter<H, A, C> {
+    pub fn iter(&self) -> Iter<'_, H, A, C> {
         Iter { claims: Some(self) }
     }
 }
